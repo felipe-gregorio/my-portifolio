348185a199d8d3864648240ebb5c8761be8b9fdf
felperaa <galvao.felipe23@gmail.com>
2023-06-09 14:01:14 -0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,70 @@
-import Image from 'next/image'
+import Head from "next/head";
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <Head>
+        <title>Felipe Gregorio Portifolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="font-bagel text-xl">Portifolio</h1>
+            <ul className="flex items-center">
+              <li></li>
+              <li>
+                <a className="bg-cyan-800  px-4 py-2 rounded-md ml-12" href="#">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 font-medium">Felipe Gregorio</h2>
+            <h3 className="text-2xl py-2">Computer Engineering Student </h3>
+            <p className="text-justify py-5 leading-6">
+              Felipe Gregorio é um estudante de Engenharia da Computação na
+              Universidade Tecnológica Federal do Paraná (UTFPR). Sua jornada na
+              computação começou desde cedo, mas foi no ensino médio que ele
+              decidiu que seria sua carreira e confirmou isso durante a
+              graduação. Embora ainda seja um estudante, Felipe possui uma
+              sólida base de conhecimento e habilidades em diversas áreas. Como
+              um dos fundadores da empresa júnior Therion, ele atualmente ocupa
+              o cargo de diretor de projetos. Essa experiência lhe proporcionou
+              o desenvolvimento de habilidades em gerenciamento de projetos e
+              liderança de equipes. Ao longo da graduação, ele também adquiriu
+              conhecimento em linguagens de programação como Java, C/C++,
+              Python, JavaScript, HTML e CSS. Além disso, ele trabalhou com
+              microcontroladores como Arduino, aplicando seus conhecimentos em
+              projetos de extensão com impacto social. Felipe é conhecido por
+              sua habilidade de se comunicar de forma eficaz e sua honestidade.
+              Ele é um excelente colaborador em equipes, demonstrando tanto
+              liderança quanto habilidades de seguir liderança. Sua
+              determinação, respeito, comprometimento e organização são
+              características valorizadas por ele, as quais ele mantém com
+              perseverança. Com sua paixão pela tecnologia, habilidades técnicas
+              e qualidades pessoais, Felipe se destaca como um
+              profissional promissor no campo da Engenharia da Computação.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16">
+           <a href="https://github.com/felipe-gregorio" target="_blank" rel="noopener noreferrer" className="text-5xl">
+            <AiFillGithub />
+            </a> 
+            <a href="https://www.linkedin.com/in/felipeggregorio/" target="_blank" rel="noopener noreferrer" className="text-5xl">
+              <AiFillLinkedin />
+              </a>
+            <a href="https://www.instagram.com/felpsgregorio/" target="_blank" rel="noopener noreferrer" className="text-5xl">
+              <AiFillInstagram />
+              </a>
+          </div>
+          <div className="relative w-48 h-48 mx-auto mt-8 rounded-full overflow-hidden">
+            <img className="object-cover w-full h-full" src="/felps.png" alt="Felipe Gregorio"/>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
